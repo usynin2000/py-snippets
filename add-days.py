451a8fa297028d2d95d3fefc197fc35f095ed0f4
Usynin Sergei <usynin.s00@mail.ru>
2025-02-20 20:01:15 +0300

    dates.

--- add-days.py
+++ add-days.py
@@ -5,10 +5,27 @@ def add_days(n: int, date: datetime) -> datetime:
     return date + timedelta(n)
 
 
-print(add_days(3, datetime.now()))
-print(add_days(-30, datetime.now()))
 
-# Calculates the date of n days from the given date.
-#
-# Use datetime.timedelta and the + operator to calculate the new datetime.datetime value after adding n days to d.
-# Omit the second argument, d, to use a default value of datetime.today().
+def format_date_to_str(date: datetime) -> str:
+    return date.strftime("%H:%M:%S %d.%m.%Y")
+    # can be any format u need
+    # %y-%Y depends on what format u need 25 or 2025
+    # %h or %m Feb or 02 as a mounth
+
+
+def format_str_to_date(date_str: str) -> datetime:
+    return datetime.strptime(date_str, "%H:%M:%S %d.%m.%Y")
+
+
+
+if __name__ == "__main__":
+    date = datetime.now()
+    print(add_days(3, date))
+    print(add_days(-30, date))
+
+
+    print(format_date_to_str(date))
+
+    date_str = format_date_to_str(date)
+
+    print(format_str_to_date(date_str))
