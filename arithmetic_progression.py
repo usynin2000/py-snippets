ef32ee99302d4c964cb7a3596a30d7fb6dd1ddf6
Usynin Sergei <usynin.s00@mail.ru>
2025-03-04 23:14:09 +0300

    Add arithmetic_progression.

---
+++ arithmetic_progression.py
@@ -0,0 +1,11 @@
+# Generates a list of numbers in the arithmetic progression starting with the given positive integer and up to the specified limit.
+#
+# Use range() and list() with the appropriate start, step and end values.
+
+
+def arithmetic_progression(n, limit):
+    return list(range(n, limit +1, n))
+
+
+if __name__ == '__main__':
+    print(arithmetic_progression(5, 25))
