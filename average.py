23ccb8fe8b14e2d85c159b61e11aab0066253f00
Usynin Sergei <usynin.s00@mail.ru>
2025-03-07 17:04:33 +0300

    Add average func.

---
+++ average.py
@@ -0,0 +1,13 @@
+
+
+def average(*args):
+    return sum(args, 0.0)/len(args)
+## 0.0 to make it float
+
+
+
+
+if __name__ == '__main__':
+    print(average(*[1, 2, 3]))
+    print(average(1, 2, 3))
+    print(average(1, 2, 5))
