ad873d967311f080af154cb2f830e6ca4a18e4c6
Usynin Sergei <usynin.s00@mail.ru>
2025-03-16 23:25:14 +0300

    Add curry_func

---
+++ curry.py
@@ -0,0 +1,22 @@
+from functools import partial
+
+# Удобство повторного использования:
+# можно заранее зафиксировать часто используемые аргументы и переиспользовать функцию.
+
+
+def curry(fn, *args):
+    return partial(fn, *args)
+
+
+
+if __name__ == "__main__":
+    add = lambda x, y: x + y
+
+    add10 = curry(add, 10)
+
+    print(add10(20))
+
+
+    add200 = curry(add, 200)
+
+    print(add200(300))
