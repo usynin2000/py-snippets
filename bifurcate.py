a550936876a5eb47b3d52c3c3cde8cb004cddec2
Usynin Sergei <usynin.s00@mail.ru>
2025-03-22 15:46:18 +0300

    Add zip magic.

--- bifurcate.py
+++ bifurcate.py
@@ -7,7 +7,18 @@ def bifurcate_by(lst: list, fn: Callable) -> list:
         [x for x in lst if not fn(x)]
     ]
 
+def bifurcate(lst: list, filter: list) -> list[list]:
+    return [
+        [x for x, flag in zip(lst, filter) if flag],
+        [x for x, flag in zip(lst, filter) if not flag]
+    ]
+
 if __name__ == "__main__":
     print(
         bifurcate_by(["boob", "bar", "foo", "hey"], lambda x: x[0] == "b")
     )
+
+    lst_1 = ["beep", "boop", "zip", "zap", 3]
+    lst_2 =[True, False, True, True, False]
+
+    print(bifurcate(lst_1, lst_2))
---
+++ zip_magic.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+def ez_example(lst_1: list, lst_2: list) -> Any:
+    zipped = zip(lst_1, lst_2)
+    return list(zipped)
+
+def iterator_example(lst_1: list, lst_2: list) -> Any:
+    zipped = zip(lst_1, lst_2)
+    return zipped
+
+
+
+
+
+if __name__ == "__main__":
+    names = ["Serega", "Narek", "Zheka", "Max"]
+    ages = [100, 28, 300, 22]
+    print(ez_example(names, ages))
+
+    # example of zip shortened
+    ages = [20, 30, 40]
+    print(ez_example(names, ages))
+
+    zipped = iterator_example(names, ages)
+    print(next(zipped))
+    print(next(zipped))
+    print(next(zipped))
+    names = ["Serega", "Any"]
+    ages = [24, 18]
+    zipped = [('Alice', 25), ('Bob', 30), ('Charlie', 35)]
+    names, ages = zip(*zipped)
+
+    print(names)
+    print(ages)
+
