6656e61572b02c8434f91a1ada1c452b4ac11e64
Usynin Sergei <usynin.s00@mail.ru>
2025-03-26 22:00:59 +0300

    Add factorial recursion func.

--- recursion.py
+++ recursion.py
@@ -8,8 +8,15 @@ def deep_flatten(lst) -> list:
         if isinstance(lst, Iterable) else [lst]
     )
 
+def factorial(n: int) -> int:
+    if not ((n >= 0) and (n % 1 == 0)):
+        raise Exception("Number cannot bet floating point or negative")
+    return 1 if n == 0 else n * factorial(n - 1)
+
 
 if __name__ == "__main__":
     bad_list = [1, [2], [2, 4], [[3, [10]]] ]
 
     print(deep_flatten(bad_list))
+
+    print(factorial(6))
