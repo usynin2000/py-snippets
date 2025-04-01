96c04b773bb761707d6fc878689df3aed1a5f30b
Usynin Sergei <usynin.s00@mail.ru>
2025-04-01 19:30:58 +0300

    Add prime_number func.

---
+++ prime_number.py
@@ -0,0 +1,19 @@
+from math import sqrt
+
+
+
+
+
+def is_prime(n: int) -> bool:
+    if n <= 0 or (n % 2 == 0 and n > 3):
+        return False
+    for i in range(3, int(sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    print(is_prime(3))
+    print(is_prime(10))
+    print(is_prime(121))
+    print(is_prime(7))
