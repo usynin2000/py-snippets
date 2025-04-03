ac3298dfadd6a715efaeed2cbfd154a70e6b8c28
Usynin Sergei <usynin.s00@mail.ru>
2025-04-03 20:47:00 +0300

    Add for_each.py

---
+++ for_each.py
@@ -0,0 +1,13 @@
+from typing import Iterable, Callable, Any
+
+
+def for_each(itr: Iterable, func: Callable) -> Any:
+    for el in itr:
+        func(el)
+
+
+
+if __name__ == "__main__":
+    for_each([1, 2, 3, 4, 5], print)
+
+
