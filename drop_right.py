5abba2c71dae65abe8afa9daf0a0a91b07ea4ade
Usynin Sergei <usynin.s00@mail.ru>
2025-04-05 21:54:21 +0300

    Add take func.

--- drop_right.py
+++ drop_right.py
@@ -3,6 +3,11 @@
 def drop_right(lst: list, n: int = 1) -> list:
     return lst[:-n]
 
+def take(lst: list, n: int ) -> list:
+    return lst[:n]
+
 if __name__ == "__main__":
     print(drop_right([1, 2, 3]))
     print(drop_right([1, 2, 3, 4, 5], 3))
+
+    print(take([1, 2, 3, 4, 5], 2))
