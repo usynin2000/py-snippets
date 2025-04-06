1baf5eb9869aec06175caf05b145e8af6c3d010c
Usynin Sergei <usynin.s00@mail.ru>
2025-04-06 11:29:18 +0300

    Add check_prop

---
+++ check_prop.py
@@ -0,0 +1,14 @@
+from typing import Callable, Any
+
+
+def check_prop(fn: Callable, prop: Any) -> Any:
+    return lambda obj: fn(obj[prop])
+
+
+
+
+if __name__ == "__main__":
+
+    check_age = check_prop(lambda x: x >= 18, "age")
+    user = {"user": "Mark", "age": 18}
+    print(check_age(user))
