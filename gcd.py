bb1997bc3b2649cbcd55f2a781313fb44d4dcb39
Usynin Sergei <usynin.s00@mail.ru>
2025-04-13 15:31:09 +0300

    Greatest common divider.

---
+++ gcd.py
@@ -0,0 +1,13 @@
+
+### Greatest common divider
+from functools import reduce
+from math import gcd as gcd_
+
+
+def gcd(numbers: list[int]) -> int:
+    return int(reduce(gcd_, numbers))
+
+
+if __name__ == "__main__":
+    print(gcd([8, 36, 28]))
+    print(gcd([20, 15, 45]))
