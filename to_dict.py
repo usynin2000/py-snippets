a670e279afd32eda204adf8820065dee68928c23
Usynin Sergei <usynin.s00@mail.ru>
2025-04-14 20:12:17 +0300

    to dict.

---
+++ to_dict.py
@@ -0,0 +1,10 @@
+
+
+
+def to_dict(keys: list, values: list) -> dict:
+    return dict(zip(keys, values))
+
+
+
+if __name__ == "__main__":
+    print(to_dict(["ke1", "key2"], ["value1", "value2"]))
