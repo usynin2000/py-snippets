419c7d8f9ec5f45a0ec176becf04266e10f1bc3c
Usynin Sergei <usynin.s00@mail.ru>
2025-04-17 20:08:23 +0300

    is_anagram.

---
+++ is_anagram.py
@@ -0,0 +1,10 @@
+
+
+
+def is_anagram(str1: str, str2: str) -> bool:
+    return sorted(str1) == sorted(str2)
+
+
+if __name__ == '__main__':
+    print(is_anagram("racecar", "carrace"))
+    print(is_anagram("racecar", "ca2rrace"))
