612fefd124c278b7f5f59538ae92d082665379d6
Usynin Sergei <usynin.s00@mail.ru>
2025-04-23 18:23:26 +0400

    ADD palindrome.

---
+++ palindrome.py
@@ -0,0 +1,19 @@
+
+
+def is_palindrome(s: str) -> bool:
+    new_s = ""
+    for c in s:
+        if c.isalnum():
+            new_s += c.lower()
+
+    return new_s == new_s[::-1]
+
+
+
+
+
+if __name__ == "__main__":
+    s = "Was it a car or a cat I saw?"
+    print(is_palindrome(s))
+    s = "tab a cat"
+    print(is_palindrome(s))
