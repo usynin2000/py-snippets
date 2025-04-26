5cdc576f8e9673469c86fb0802aaf0b1475593a6
Usynin Sergei <usynin.s00@mail.ru>
2025-04-26 14:12:16 +0400

    Another max_profit.

--- max_profit.py
+++ max_profit.py
@@ -1,6 +1,5 @@
 
 
-
 def max_profit_brute_force(prices: list[int]) -> int:
     res = 0
     for i in range(len(prices)):
@@ -13,6 +12,21 @@ def max_profit_brute_force(prices: list[int]) -> int:
     return res
 
 
+def max_profit_l_r(prices: list[int]) -> int:
+    l, r = 0, 1
+    max_profit = 0
+    while r < len(prices):
+        if prices[l] < prices[r]:
+            max_profit = max(max_profit, prices[r] - prices[l])
+        else:
+            l = r
+        r += 1
+
+    return max_profit
+
+
+
 if __name__ == "__main__":
     prices = [7, 1, 5, 3, 6, 4]
     print(max_profit_brute_force(prices))
+    print(max_profit_l_r(prices))
