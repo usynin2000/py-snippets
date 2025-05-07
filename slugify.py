bfc23f92788a78ec3c87da4500801cc7edba2ff7
Usynin Sergei <usynin.s00@mail.ru>
2025-05-07 21:03:56 +0300

    halyava

---
+++ slugify.py
@@ -0,0 +1,16 @@
+import re
+
+
+
+def slugify(s: str) -> str:
+    s = s.lower().strip()
+    s = re.sub(r'[^\w\s-]', '', s)
+    s = re.sub(r'[\s_-]+', '-', s)
+    s = re.sub(r'^-+|-+$', '', s)
+    return s
+
+
+
+
+if __name__ == "__main__":
+    print(slugify('Hello World!'))
