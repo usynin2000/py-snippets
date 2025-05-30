abe308ae74f615b9ad3f629185e9c3a6ef5758da
Usynin Sergei <usynin.s00@mail.ru>
2025-05-30 19:25:32 +0300

    Add s3_reading

---
+++ s3_reading.py
@@ -0,0 +1,36 @@
+from urllib.request import urlopen
+import time
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+url = os.getenv('S3_URL')
+
+
+def process_user_ids(user_ids):
+    print(f"Processing batch of {len(user_ids)} users")
+    # Здесь может быть любой код: запрос в Mongo, API вызов и т.д.
+
+batch_size = 300
+user_ids = []
+
+with urlopen(url) as response:
+    next(response)  # <- пропускаем первую строку с "account_id"
+
+    for line in response:
+        user_id = line.decode("utf-8").strip()
+        print(user_id)
+        time.sleep(0.2)
+        if user_id:
+            user_ids.append(user_id)
+
+            if len(user_ids) == batch_size:
+                process_user_ids(user_ids)
+                user_ids = []
+                time.sleep(0.2)
+
+if user_ids:
+    process_user_ids(user_ids)
