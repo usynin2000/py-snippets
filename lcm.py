a5edf760e2519174f4fb33f852427a633580d2b9
Usynin Sergei <usynin.s00@mail.ru>
2025-06-09 23:10:13 +0300

    Add explanation to lcm

--- lcm.py
+++ lcm.py
@@ -3,6 +3,12 @@ from math import gcd
 
 # Returns the least common multiple of a list of numbers.
 
+# reduce — применяет функцию к элементам последовательности по порядку, сводя список к одному значению.
+# В reduce(...) мы передаём лямбда-функцию:
+# lambda x, y: int(x * y / gcd(x, y))
+# которая берёт два числа x и y, находит их НОК и возвращает его.
+# reduce будет применять эту функцию ко всем элементам списка слева направо:
+
 def lcm(numbers):
     return reduce(
         (
