116185cbe0f0ea1e1fb6b340acba1e59eb20f551
Usynin Sergei <usynin.s00@mail.ru>
2025-07-06 23:24:33 +0300

    Add signature of merge intervals func.

--- top_50_leetcode_problems.py
+++ top_50_leetcode_problems.py
@@ -82,6 +82,7 @@ def top_k_frequent(nums: list, k: int) -> list:
         result.append(freg_list[i][1])
     return result
 
+
 # Product of Array Except Self
 # Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 #
@@ -100,8 +101,16 @@ def top_k_frequent(nums: list, k: int) -> list:
 
 
 
-
-
+# Merge Intervals
+# Given an array of intervals where intervals[i] = [starti, endi],
+# merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+# Example 1:
+#
+# Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+# Output: [[1,6],[8,10],[15,18]]
+# Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+def merge_intervals(intervals:list[list]) -> list[list]:
+    pass
 
 
 if __name__ == "__main__":
