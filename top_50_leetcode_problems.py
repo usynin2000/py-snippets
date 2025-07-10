88498b848eefb6426eb8eefce4f6a25d9813e779
Usynin Sergei <usynin.s00@mail.ru>
2025-07-10 22:39:28 +0300

    Add max_sub_array.

--- top_50_leetcode_problems.py
+++ top_50_leetcode_problems.py
@@ -126,6 +126,39 @@ def merge_intervals(intervals:list[list]) -> list[list]:
     return merged
 
 
+# Given an integer array nums, find the subarray with the largest sum, and return its sum.
+# Example 1:
+#
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+# Example 2:
+#
+# Input: nums = [1]
+# Output: 1
+# Explanation: The subarray [1] has the largest sum 1.
+# Example 3:
+#
+# Input: nums = [5,4,-1,7,8]
+# Output: 23
+# Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+#
+
+def max_sub_array(nums: list) -> int:
+    res = nums[0]
+    total = 0
+
+    for n in nums:
+        if total < 0:
+            total = 0
+
+        total += n
+        res = max(res, total)
+
+    return res
+
+
+
 if __name__ == "__main__":
     print(two_sum([2, 7, 11, 15], 9))
 
@@ -137,3 +170,5 @@ if __name__ == "__main__":
     print(top_k_frequent([1,1,1,2,2,3],2 ))
     print(top_k_frequent([1,1,1,2,2,3, 4, 5],3 ))
     print(merge_intervals([[1, 2], [2, 4]]))
+    print(max_sub_array([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+    print(max_sub_array([5,4,-1,7,8]))
