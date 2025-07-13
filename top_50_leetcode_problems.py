cc81d54fb401b3f63a95eec1aba698d6f8ebfd40
Usynin Sergei <usynin.s00@mail.ru>
2025-07-13 18:00:38 +0300

    Add two_sum sorted array.

--- top_50_leetcode_problems.py
+++ top_50_leetcode_problems.py
@@ -169,6 +169,33 @@ def is_palindrome(s: str) -> bool:
     ]
     return filtered == filtered[::-1]
 
+# ### 12. Two Sum II - Input Array Is Sorted
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+#
+# Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+#
+# The tests are generated such that there is exactly one solution. You may not use the same element twice.
+#
+# # Your solution must use only constant extra space.
+#
+# Input: numbers = [2,7,11,15], target = 9
+# Output: [1,2]
+# Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2]
+
+def two_sum_with_sorted_array(numbers: list[int], target: int) -> list[int]:
+    left, right = 0, len(numbers) - 1
+
+    while left < right:
+        sum = numbers[left] + numbers[right]
+
+        if sum == target:
+            return [left + 1, right + 1]
+        elif sum < target:
+            left += 1
+        else:
+            right -= 1
+
+
 
 if __name__ == "__main__":
     print(two_sum([2, 7, 11, 15], 9))
@@ -188,3 +215,6 @@ if __name__ == "__main__":
     print(is_palindrome(s))
     s = "tab a cat"
     print(is_palindrome(s))
+    print(two_sum_with_sorted_array([2,7,11,15], 9))
+    print(two_sum_with_sorted_array([2,3,4], 6))
+    print(two_sum_with_sorted_array([-1,0], -1))
