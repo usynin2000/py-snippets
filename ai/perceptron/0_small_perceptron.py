6f08cde47f77af18c3d6214c95690b2787874a69
Usynin Sergei <usynin.s00@mail.ru>
2026-01-18 20:36:15 +0300

    Rm ai folder to other repo.

--- ai/perceptron/0_small_perceptron.py
+++
@@ -1,9 +0,0 @@
-x = 1        # input: fact or not
-w = 0.2      # importance of the fact (weight) We can change it to make the perceptron more or less sensitive to the input
-b = -0.5     # threshold / bias (порого или смещение )
-
-s = x * w + b 
-
-y = 1 if s >= 0 else 0
-
-print(y)
